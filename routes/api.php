6318df4d43d17900cf35cd6ba2ff44f251c01f96
zerobit <pst@zerobit.one>
2023-05-21 13:00:42 +0200

    adjusted routes

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,17 @@
 <?php
 
 use App\Http\Controllers\API\ActivityController;
+use App\Http\Controllers\API\AuthorController;
+use App\Http\Controllers\API\AuthorRatingController;
+use App\Http\Controllers\API\NoteController;
+use App\Http\Controllers\API\OpportunityController;
+use App\Http\Controllers\API\OrganizationController;
+use App\Http\Controllers\API\RoleController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
+use App\Http\Controllers\API\CostumerController;
 use Illuminate\Support\Facades\Route;
 
-//use App\Http\Controllers\CrmController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,62 +29,62 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // Customers routes
-Route::get('/customers/{id}', [CrmController::class, 'getCustomer']);
-Route::post('/customers', [CrmController::class, 'createCustomer']);
-Route::put('/customers/{id}', [CrmController::class, 'updateCustomer']);
-Route::delete('/customers/{id}', [CrmController::class, 'deleteCustomer']);
+Route::get('/customers/{id}', [CostumerController::class, 'getCustomer']);
+Route::post('/customers', [CostumerController::class, 'createCustomer']);
+Route::put('/customers/{id}', [CostumerController::class, 'updateCustomer']);
+Route::delete('/customers/{id}', [CostumerController::class, 'deleteCustomer']);
 
 // Contacts routes
-Route::get('/contacts/{id}', [CrmController::class, 'getContact']);
-Route::post('/contacts', [CrmController::class, 'createContact']);
-Route::put('/contacts/{id}', [CrmController::class, 'updateContact']);
-Route::delete('/contacts/{id}', [CrmController::class, 'deleteContact']);
+Route::get('/contacts/{id}', [CostumerController::class, 'getContact']);
+Route::post('/contacts', [CostumerController::class, 'createContact']);
+Route::put('/contacts/{id}', [CostumerController::class, 'updateContact']);
+Route::delete('/contacts/{id}', [CostumerController::class, 'deleteContact']);
 
 // Organizations routes
-Route::get('/organizations/{id}', [CrmController::class, 'getOrganization']);
-Route::post('/organizations', [CrmController::class, 'createOrganization']);
-Route::put('/organizations/{id}', [CrmController::class, 'updateOrganization']);
-Route::delete('/organizations/{id}', [CrmController::class, 'deleteOrganization']);
+Route::get('/organizations/{id}', [OrganizationController::class, 'getOrganization']);
+Route::post('/organizations', [OrganizationController::class, 'createOrganization']);
+Route::put('/organizations/{id}', [OrganizationController::class, 'updateOrganization']);
+Route::delete('/organizations/{id}', [OrganizationController::class, 'deleteOrganization']);
 
 // Opportunities routes
-Route::get('/opportunities/{id}', [ActivityController::class, 'getOpportunity']);
-Route::post('/opportunities', [ActivityController::class, 'createOpportunity']);
-Route::put('/opportunities/{id}', [ActivityController::class, 'updateOpportunity']);
-Route::delete('/opportunities/{id}', [ActivityController::class, 'deleteOpportunity']);
+Route::get('/opportunities/{id}', [OpportunityController::class, 'getOpportunity']);
+Route::post('/opportunities', [OpportunityController::class, 'createOpportunity']);
+Route::put('/opportunities/{id}', [OpportunityController::class, 'updateOpportunity']);
+Route::delete('/opportunities/{id}', [OpportunityController::class, 'deleteOpportunity']);
 
 // Activities routes
-Route::get('/activities/{id}', [CrmController::class, 'getActivity']);
-Route::post('/activities', [CrmController::class, 'createActivity']);
-Route::put('/activities/{id}', [CrmController::class, 'updateActivity']);
-Route::delete('/activities/{id}', [CrmController::class, 'deleteActivity']);
+Route::get('/activities/{id}', [ActivityController::class, 'getActivity']);
+Route::post('/activities', [ActivityController::class, 'createActivity']);
+Route::put('/activities/{id}', [ActivityController::class, 'updateActivity']);
+Route::delete('/activities/{id}', [ActivityController::class, 'deleteActivity']);
 
 // Authors routes
-Route::get('/authors/{id}', [CrmController::class, 'getAuthor']);
-Route::post('/authors', [CrmController::class, 'createAuthor']);
-Route::put('/authors/{id}', [CrmController::class, 'updateAuthor']);
-Route::delete('/authors/{id}', [CrmController::class, 'deleteAuthor']);
+Route::get('/authors/{id}', [AuthorController::class, 'getAuthor']);
+Route::post('/authors', [AuthorController::class, 'createAuthor']);
+Route::put('/authors/{id}', [AuthorController::class, 'updateAuthor']);
+Route::delete('/authors/{id}', [AuthorController::class, 'deleteAuthor']);
 
 // Author Ratings routes
-Route::get('/author-ratings/{id}', [CrmController::class, 'getAuthorRating']);
-Route::post('/author-ratings', [CrmController::class, 'createAuthorRating']);
-Route::put('/author-ratings/{id}', [CrmController::class, 'updateAuthorRating']);
-Route::delete('/author-ratings/{id}', [CrmController::class, 'deleteAuthorRating']);
+Route::get('/author-ratings/{id}', [AuthorRatingController::class, 'getAuthorRating']);
+Route::post('/author-ratings', [AuthorRatingController::class, 'createAuthorRating']);
+Route::put('/author-ratings/{id}', [AuthorRatingController::class, 'updateAuthorRating']);
+Route::delete('/author-ratings/{id}', [AuthorRatingController::class, 'deleteAuthorRating']);
 
 // Notes routes
-Route::get('/notes/{id}', [CrmController::class, 'getNote']);
-Route::post('/notes', [CrmController::class, 'createNote']);
-Route::put('/notes/{id}', [CrmController::class, 'updateNote']);
-Route::delete('/notes/{id}', [CrmController::class, 'deleteNote']);
+Route::get('/notes/{id}', [NoteController::class, 'getNote']);
+Route::post('/notes', [NoteController::class, 'createNote']);
+Route::put('/notes/{id}', [NoteController::class, 'updateNote']);
+Route::delete('/notes/{id}', [NoteController::class, 'deleteNote']);
 
 // Users routes
-Route::get('/users/{id}', [CrmController::class, 'getUser']);
-Route::post('/users', [CrmController::class, 'createUser']);
-Route::put('/users/{id}', [CrmController::class, 'updateUser']);
-Route::delete('/users/{id}', [CrmController::class, 'deleteUser']);
+Route::get('/users/{id}', [UserController::class, 'getUser']);
+Route::post('/users', [UserController::class, 'createUser']);
+Route::put('/users/{id}', [UserController::class, 'updateUser']);
+Route::delete('/users/{id}', [UserController::class, 'deleteUser']);
 
 // Roles routes
-Route::get('/roles/{id}', [CrmController::class, 'getRole']);
-Route::post('/roles', [CrmController::class, 'createRole']);
-Route::put('/roles/{id}', [CrmController::class, 'updateRole']);
-Route::delete('/roles/{id}', [CrmController::class, 'deleteRole']);
+Route::get('/roles/{id}', [RoleController::class, 'getRole']);
+Route::post('/roles', [RoleController::class, 'createRole']);
+Route::put('/roles/{id}', [RoleController::class, 'updateRole']);
+Route::delete('/roles/{id}', [RoleController::class, 'deleteRole']);
 
